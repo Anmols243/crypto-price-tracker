ea1a33fa31cd5913ca66ca131656b627c0d125c0
Anmol Singh <168528590+Anmols243@users.noreply.github.com>
2025-07-05 20:23:23 +0530

    Imporved Error handling and Added colorama

--- price.py
+++ price.py
@@ -1,29 +1,36 @@
 import requests
 import datetime
 import time
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
+
 
 url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+try:
+    while True:
+        now = datetime.datetime.now().strftime("%I:%M:%S %p, %d %b %Y")
 
-while True:
-    now = datetime.datetime.now().strftime("%I:%M:%S %p, %d %b %Y")
-
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        price = response.json()['bitcoin']['usd']
-        print(f"As of {now}, Price of bitcoin is ${price}.")
-        time.sleep(10)
-            
-    except KeyboardInterrupt:
-        print("Interupted by the user")
-        break
-    
-    except Exception as e:
-        print("Error:", e)
-        break
-    
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            price = response.json()['bitcoin']['usd']
+            print(Fore.GREEN + f"As of {now}, Price of bitcoin is ${price}.")
+            time.sleep(10)
         
-   
+        except requests.exceptions.RequestException as e:
+            short_error = str(e).split('\n')[0]
+            print(Fore.RED  + "Network/Api error:", Fore.YELLOW + short_error)
+            print(Fore.CYAN +"Retrying in 10 seconds...\n")
+            time.sleep()
+            continue
+        
+except KeyboardInterrupt:
+    print(Fore.RED + "Interrupted by the user.")
+
+        
+            
+    
         
         
         
