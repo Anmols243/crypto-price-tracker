097c4121d0ab5cb7a2e5e9646facab47dfaf0674
Anmol Singh <168528590+Anmols243@users.noreply.github.com>
2025-07-06 19:07:19 +0530

    Added Support for multiple coins(BTC, ETH, DOGE)

--- bitcoin_price_tracker.py
+++ bitcoin_price_tracker.py
@@ -3,7 +3,7 @@ Bitcoin Price Tracker CLI Tool
 Fetches real-time Bitcoin price using CoinGecko API every 20 seconds.
 Displays color-coded output with timestamp and handles errors gracefully.
 """
-
+import argparse
 import requests
 import datetime
 import time
@@ -11,8 +11,18 @@ import colorama
 from colorama import Fore
 colorama.init(autoreset=True)
 
+parser = argparse.ArgumentParser(description="Track Crypto Prices")
+parser.add_argument(
+    "-c","--coin",
+    nargs="+",
+    default=["bitcoin"],
+    help= "coins to track"
+)
+args = parser.parse_args()
+coin = args.coin[0].lower()
 
-url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+
+url = "https://api.coingecko.com/api/v3/simple/price"
 response = None
 
 try:
@@ -20,15 +30,26 @@ try:
         now = datetime.datetime.now().strftime("%I:%M:%S %p, %d %b %Y")
 
         try:
-            response = requests.get(url)
+            params ={
+                "ids": coin,
+                "vs_currencies": "usd"
+            }
+            response = requests.get(url, params=params)
+            
             if response.status_code == 429:
                 print("Rate limit hit, waiting 60 seconds...")
                 time.sleep(60)
                 continue
             
             response.raise_for_status()
-            price = response.json()['bitcoin']['usd']
-            print(Fore.GREEN + f"As of {now}, Price of bitcoin is ${price}.")
+            data = response.json()
+            
+            price = data.get(coin, {}).get("usd")
+            if price:
+                print(Fore.GREEN + f"As of {now}, Price of {coin} is ${price}.")
+            else:
+                print(Fore.RED + f"{coin.upper()} not found or is invalid")
+            
             time.sleep(20)
         
         except requests.exceptions.RequestException as e:
@@ -41,6 +62,7 @@ try:
             
 except KeyboardInterrupt:
     print(Fore.RED + "Interrupted by the user.")
+    
 
         
             
